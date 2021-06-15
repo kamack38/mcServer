77260f66c67f84ddbd1b772c7cc9ed7c20089726
Kamack38 <kamack38@gmail.com>
2021-06-15 23:09:45 +0200

    Update

--- src/download.latest.bat
+++ src/download.latest.bat
@@ -4,10 +4,10 @@ curl "https://dev.bukkit.org/projects/worldguard/files/latest" --output plugins/
 curl "https://dev.bukkit.org/projects/bettersleeping-1-13-1-15/files/latest" --output plugins/BetterSleeping.jar
 curl "https://dev.bukkit.org/projects/player-heads/files/latest" --output plugins/PlayerHeads.jar
 curl "https://github.com/MilkBowl/Vault/releases/latest/download/Vault.jar" --output plugins/Vault.jar
-curl "https://github.com/SkinsRestorer/SkinsRestorerX/releases/latest/download/SkinsRestorer.jar" --output plugins/SkinsRestorer.jar
 explorer.exe https://www.spigotmc.org/resources/graves.74208/
 explorer.exe https://essentialsx.net/downloads.html
 explorer.exe https://luckperms.net/download
 explorer.exe https://github.com/AuthMe/AuthMeReloaded/releases/tag/5.6.0-beta2
 explorer.exe https://github.com/NEZNAMY/TAB/releases
 explorer.exe https://www.spigotmc.org/resources/chunky.81534/
+explorer.exe https://github.com/SkinsRestorer/SkinsRestorerX/releases/latest/download/SkinsRestorer.jar
