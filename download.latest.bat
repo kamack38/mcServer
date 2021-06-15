22d32e8660044c89fe38e937fb3786562ac5f9dc
Kamack38 <kamack38@gmail.com>
2021-06-15 20:58:52 +0200

    Add auto download

---
+++ download.latest.bat
@@ -0,0 +1,13 @@
+curl "https://purpur.pl3x.net/api/v1/purpur/1.16.5/latest/download" --output purpur.jar
+curl "https://dev.bukkit.org/projects/worldedit/files/latest" --output plugins/WorldEdit.jar
+curl "https://dev.bukkit.org/projects/worldguard/files/latest" --output plugins/WorldGuard.jar
+curl "https://dev.bukkit.org/projects/bettersleeping-1-13-1-15/files/latest" --output plugins/BetterSleeping.jar
+curl "https://dev.bukkit.org/projects/player-heads/files/latest" --output plugins/PlayerHeads.jar
+curl "https://github.com/MilkBowl/Vault/releases/latest/download/Vault.jar" --output plugins/Vault.jar
+curl "https://github.com/SkinsRestorer/SkinsRestorerX/releases/latest/download/SkinsRestorer.jar" --output plugins/SkinsRestorer.jar
+explorer.exe https://www.spigotmc.org/resources/graves.74208/
+explorer.exe https://essentialsx.net/downloads.html
+explorer.exe https://luckperms.net/download
+explorer.exe https://github.com/AuthMe/AuthMeReloaded/releases/tag/5.6.0-beta2
+explorer.exe https://github.com/NEZNAMY/TAB/releases
+explorer.exe https://www.spigotmc.org/resources/chunky.81534/
