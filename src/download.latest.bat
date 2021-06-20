f46a0fc48257fae643c21b55973f2f7f93403b46
Kamack38 <kamack38@gmail.com>
2021-06-20 18:38:26 +0200

    v1.2

--- src/download.latest.bat
+++ src/download.latest.bat
@@ -1,13 +1,13 @@
-curl "https://purpur.pl3x.net/api/v1/purpur/1.16.5/latest/download" --output purpur.jar
-curl "https://dev.bukkit.org/projects/worldedit/files/latest" --output plugins/WorldEdit.jar
-curl "https://dev.bukkit.org/projects/worldguard/files/latest" --output plugins/WorldGuard.jar
-curl "https://dev.bukkit.org/projects/bettersleeping-1-13-1-15/files/latest" --output plugins/BetterSleeping.jar
-curl "https://dev.bukkit.org/projects/player-heads/files/latest" --output plugins/PlayerHeads.jar
-curl "https://github.com/MilkBowl/Vault/releases/latest/download/Vault.jar" --output plugins/Vault.jar
-explorer.exe https://www.spigotmc.org/resources/graves.74208/
+curl "https://purpur.pl3x.net/api/v1/purpur/1.16.5/latest/download" -o purpur.jar
+curl "https://dev.bukkit.org/projects/bettersleeping-1-13-1-15/files/latest" -o plugins/BetterSleeping.jar
+curl "https://dev.bukkit.org/projects/player-heads/files/latest" -o plugins/PlayerHeads.jar
+curl "https://github.com/MilkBowl/Vault/releases/latest/download/Vault.jar" -o plugins/Vault.jar
+curl "https://github.com/SkinsRestorer/SkinsRestorerX/releases/latest/download/SkinsRestorer.jar" -L -o plugins/SkinsRestorer.jar
+curl "https://dev.bukkit.org/projects/worldedit/files/latest" -L -o plugins/WorldEdit.jar
+curl "https://dev.bukkit.org/projects/worldguard/files/latest" -L -o plugins/WorldGuard.jar
 explorer.exe https://essentialsx.net/downloads.html
 explorer.exe https://luckperms.net/download
-explorer.exe https://github.com/AuthMe/AuthMeReloaded/releases/tag/5.6.0-beta2
-explorer.exe https://github.com/NEZNAMY/TAB/releases
+explorer.exe https://www.spigotmc.org/resources/graves.74208/
 explorer.exe https://www.spigotmc.org/resources/chunky.81534/
-explorer.exe https://github.com/SkinsRestorer/SkinsRestorerX/releases/latest/download/SkinsRestorer.jar
+explorer.exe https://github.com/AuthMe/AuthMeReloaded/releases/latest
+explorer.exe https://github.com/NEZNAMY/TAB/releases
