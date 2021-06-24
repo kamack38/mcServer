887893c90fb3851a8c8d610fcb66e83a69a0d622
Kamack38 <kamack38@gmail.com>
2021-06-24 22:37:32 +0200

    Udpate

--- src/download.latest.bat
+++ src/download.latest.bat
@@ -1,5 +1,4 @@
 curl "https://purpur.pl3x.net/api/v1/purpur/1.16.5/latest/download" -o purpur.jar
-curl "https://dev.bukkit.org/projects/player-heads/files/latest" -L -o plugins/PlayerHeads.jar
 curl "https://github.com/MilkBowl/Vault/releases/latest/download/Vault.jar" -L -o plugins/Vault.jar
 curl "https://github.com/SkinsRestorer/SkinsRestorerX/releases/latest/download/SkinsRestorer.jar" -L -o plugins/SkinsRestorer.jar
 explorer.exe https://essentialsx.net/downloads.html
@@ -11,3 +10,4 @@ explorer.exe https://github.com/NEZNAMY/TAB/releases
 explorer.exe https://dev.bukkit.org/projects/bettersleeping-1-13-1-15/files/latest
 explorer.exe https://dev.bukkit.org/projects/worldedit/files/latest
 explorer.exe https://dev.bukkit.org/projects/worldguard/files/latest
+explorer.exe https://dev.bukkit.org/projects/player-heads/files/latest
