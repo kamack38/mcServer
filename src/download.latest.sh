fbfeb29aea55238968ea51f4e9219ab28150252f
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-06-01 20:27:03 +0200

    feat(scripts): 🎸 Add colorful messages

--- src/download.latest.sh
+++ src/download.latest.sh
@@ -1,84 +1,96 @@
 #!/usr/bin/bash
 cd "$(dirname "$0")"
 
-NC='\033[0m'
-Yellow='\033[1;33m'
+BLACK=$'\e[0;30m'
+WHITE=$'\e[0;37m'
+BWHITE=$'\e[1;37m'
+RED=$'\e[0;31m'
+BLUE=$'\e[0;34m'
+GREEN=$'\e[0;32m'
+YELLOW=$'\e[0;33m'
+NC=$'\e[0m' # No Colour
 
 rm -f purpur.jar
 rm -rf plugins/*.jar
 
+i=1
+maxi=14
+
 # Download purpur
+printf "\r${BLUE}:: ${BWHITE}Downloading ${BLUE}purpur engine${BWHITE}...${NC}"
 wget -qLO purpur.jar "https://api.purpurmc.org/v2/purpur/1.18.2/latest/download"
-
-# Download WorldEdit
-wget -qLO plugins/WorldEdit.jar "https://dev.bukkit.org/projects/worldedit/files/latest"
-
-# Download WorldGuard
-wget -qLO plugins/WorldGuard.jar "https://dev.bukkit.org/projects/worldguard/files/latest"
-
-# Download PlayerHeads
-wget -qLO plugins/PlayerHeads.jar "https://dev.bukkit.org/projects/player-heads/files/latest"
-
-# Download CoreProtect
-wget -qLO plugins/CoreProtect.jar "https://dev.bukkit.org/projects/coreprotect/files/latest"
-
-# Download Graves
-wget -qLO plugins/Graves.jar "https://repo.ranull.com/maven/ranull/com/ranull/Graves/DEV/Graves-DEV.jar"
+printf "\r${GREEN}:: ${BWHITE}Succesfully downloaded ${BLUE}purpur${BWHITE}!${NC}\n"
 
 function downloadLatestReleases() {
     # Split path
-    pluginName=$(echo "${2##*/}")
-    echo -e "${Yellow}Downloading ${pluginName}...${NC}"
+    printf "\r${BLUE}:: ${BWHITE}Downloading ${BLUE}${2}${BWHITE}...${NC}"
     curl -s $1 |
         grep browser_download_url |
         cut -d : -f 2,3 |
         tr -d \" |
-        wget -O $2 -qLi -
+        wget -O "plugins/${2}.jar" -qLi -
+    printf "\r${GREEN}:: ${NC}[$i/$maxi] ${BWHITE}Succesfully downloaded ${BLUE}${2}${BWHITE}!${NC}\n"
+    ((i += 1))
+
 }
 
 # Download latest MilkBowl/Vault release from github
-downloadLatestReleases "https://api.github.com/repos/MilkBowl/Vault/releases" "plugins/Vault.jar"
+downloadLatestReleases "https://api.github.com/repos/MilkBowl/Vault/releases" "Vault"
 
 # Download latest SkinsRestorer/SkinsRestorerX release from github
-downloadLatestReleases "https://api.github.com/repos/SkinsRestorer/SkinsRestorerX/releases/latest" "plugins/SkinsRestorerX.jar"
+downloadLatestReleases "https://api.github.com/repos/SkinsRestorer/SkinsRestorerX/releases/latest" "SkinsRestorerX"
 
 # Download latest Nuytemans-Dieter/BetterSleeping release from github
-downloadLatestReleases "https://api.github.com/repos/Nuytemans-Dieter/BetterSleeping/releases/latest" "plugins/BetterSleeping.jar"
+downloadLatestReleases "https://api.github.com/repos/Nuytemans-Dieter/BetterSleeping/releases/latest" "BetterSleeping"
 
 # Download latest NEZNAMY/TAB release from github
-downloadLatestReleases "https://api.github.com/repos/NEZNAMY/TAB/releases/latest" "plugins/TAB.jar"
+downloadLatestReleases "https://api.github.com/repos/NEZNAMY/TAB/releases/latest" "TAB"
 
 # Download latest AuthMe/AuthMeReloaded release from github
-downloadLatestReleases "https://api.github.com/repos/AuthMe/AuthMeReloaded/releases/latest" "plugins/AuthMeReloaded.jar"
+downloadLatestReleases "https://api.github.com/repos/AuthMe/AuthMeReloaded/releases/latest" "AuthMeReloaded"
 
 # Download latest LuckPerms
-downloadLatestReleases "https://metadata.luckperms.net/data/downloads" "plugins/LuckPerms.jar"
+downloadLatestReleases "https://metadata.luckperms.net/data/downloads" "LuckPerms"
 
-function downloadLatestFile() {
+function downloadLatestBuild() {
+    printf "\r${BLUE}:: ${BWHITE}Downloading ${BLUE}${2}${BWHITE}...${NC}"
     curl -s $1 |
-        grep -Po $2 |
+        grep -Po "$2-.*?\.jar" |
         head -n 1 |
-        sed "s/^/${1}/" |
-        wget -O "plugins/${3}" -qi -
+        sed "s#^#${1}#" |
+        wget -O "plugins/${2}.jar" -qi -
+    printf "\r${GREEN}:: ${NC}[$i/$maxi] ${BWHITE}Succesfully downloaded ${BLUE}${2}${BWHITE}!${NC}\n"
+    ((i += 1))
+
 }
 
 # Download EssenstialsX
-curl -s "https://ci.ender.zone/job/EssentialsX/lastSuccessfulBuild/artifact/jars/" |
-    grep -Po "EssentialsX-.*?\.jar" |
-    head -n 1 |
-    sed 's/^/https:\/\/ci.ender.zone\/job\/EssentialsX\/lastSuccessfulBuild\/artifact\/jars\//' |
-    wget -O plugins/EssentialsX.jar -qi -
+downloadLatestBuild "https://ci.ender.zone/job/EssentialsX/lastSuccessfulBuild/artifact/jars/" "EssentialsX"
 
 # Download EssenstialsXChat
-curl -s "https://ci.ender.zone/job/EssentialsX/lastSuccessfulBuild/artifact/jars/" |
-    grep -Po "EssentialsXChat-.*?\.jar" |
-    head -n 1 |
-    sed 's/^/https:\/\/ci.ender.zone\/job\/EssentialsX\/lastSuccessfulBuild\/artifact\/jars\//' |
-    wget -O plugins/EssenstialsXChat.jar -qi -
+downloadLatestBuild "https://ci.ender.zone/job/EssentialsX/lastSuccessfulBuild/artifact/jars/" "EssenstialsXChat"
 
 # Download Chunky
-curl -s "https://ci.codemc.io/view/Author/job/pop4959/job/Chunky/lastSuccessfulBuild/artifact/bukkit/build/libs/" |
-    grep -Po "Chunky-.*?\.jar" |
-    head -n 1 |
-    sed 's/^/https:\/\/ci.codemc.io\/view\/Author\/job\/pop4959\/job\/Chunky\/lastSuccessfulBuild\/artifact\/bukkit\/build\/libs\//' |
-    wget -O plugins/Chunky.jar -qi -
+downloadLatestBuild "https://ci.codemc.io/view/Author/job/pop4959/job/Chunky/lastSuccessfulBuild/artifact/bukkit/build/libs/" "Chunky"
+
+function downloadLatestFile() {
+    printf "\r${BLUE}:: ${BWHITE}Downloading ${BLUE}${2}${BWHITE}...${NC}"
+    wget -qLO "plugins/${2}.jar" "${1}"
+    printf "\r${GREEN}:: ${NC}[$i/$maxi] ${BWHITE}Succesfully downloaded ${BLUE}${2}${BWHITE}!${NC}\n"
+    ((i += 1))
+}
+
+# Download WorldEdit
+downloadLatestFile "https://dev.bukkit.org/projects/worldedit/files/latest" "WorldEdit"
+
+# Download WorldGuard
+downloadLatestFile "https://dev.bukkit.org/projects/worldguard/files/latest" "WorldGuard"
+
+# Download PlayerHeads
+downloadLatestFile "https://dev.bukkit.org/projects/player-heads/files/latest" "PlayerHeads"
+
+# Download CoreProtect
+downloadLatestFile "https://dev.bukkit.org/projects/coreprotect/files/latest" "CoreProtect"
+
+# Download Graves
+downloadLatestFile "https://repo.ranull.com/maven/ranull/com/ranull/Graves/DEV/Graves-DEV.jar" "Graves"
