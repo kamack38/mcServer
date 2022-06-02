b38281ae0e8693cbeaeb6c85eff7d5b55fa8c95b
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-06-02 22:34:51 +0200

    feat: 🎸 Fix update scripts & update configs

--- src/download.latest.sh
+++ src/download.latest.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 cd "$(dirname "$0")"
 
+set -e
+
 BLACK=$'\e[0;30m'
 WHITE=$'\e[0;37m'
 BWHITE=$'\e[1;37m'
@@ -50,7 +52,11 @@ downloadLatestReleases "https://api.github.com/repos/NEZNAMY/TAB/releases/latest
 downloadLatestReleases "https://api.github.com/repos/AuthMe/AuthMeReloaded/releases/latest" "AuthMeReloaded"
 
 # Download latest LuckPerms
-downloadLatestReleases "https://metadata.luckperms.net/data/downloads" "LuckPerms"
+printf "\r${BLUE}:: ${BWHITE}Downloading ${BLUE}LuckPerms${BWHITE}...${NC}"
+LP_URL=$(curl -s https://metadata.luckperms.net/data/downloads | jq '.downloads.bukkit' -r)
+wget -qO "plugins/LuckPerms.jar" $LP_URL
+printf "\r${GREEN}:: ${NC}[$i/$maxi] ${BWHITE}Succesfully downloaded ${BLUE}LuckPerms${BWHITE}!${NC}\n"
+((i += 1))
 
 function downloadLatestBuild() {
     printf "\r${BLUE}:: ${BWHITE}Downloading ${BLUE}${2}${BWHITE}...${NC}"
@@ -61,14 +67,24 @@ function downloadLatestBuild() {
         wget -O "plugins/${2}.jar" -qi -
     printf "\r${GREEN}:: ${NC}[$i/$maxi] ${BWHITE}Succesfully downloaded ${BLUE}${2}${BWHITE}!${NC}\n"
     ((i += 1))
-
 }
 
-# Download EssenstialsX
-downloadLatestBuild "https://ci.ender.zone/job/EssentialsX/lastSuccessfulBuild/artifact/jars/" "EssentialsX"
+function downloadEssentialsX() {
+    res=$(curl -s "https://ci-api.essentialsx.net/job/EssentialsX/lastSuccessfulBuild/api/json")
+    url=$(echo $res | jq '.url' -r)
+    path1=$(echo $res | jq '.artifacts[0].relativePath' -r) # Get EssentialX
+    path2=$(echo $res | jq '.artifacts[2].relativePath' -r) # Get EssentialXChat
+    printf "\r${BLUE}:: ${BWHITE}Downloading ${BLUE}EssentialsX${BWHITE}...${NC}"
+    wget -qO "plugins/EssentialsX.jar" "${url}artifact/${path1}"
+    printf "\r${GREEN}:: ${NC}[$i/$maxi] ${BWHITE}Succesfully downloaded ${BLUE}EssentialsX${BWHITE}!${NC}\n"
+    ((i += 1))
+    printf "\r${BLUE}:: ${BWHITE}Downloading ${BLUE}EssenstialsXChat${BWHITE}...${NC}"
+    wget -qO "plugins/EssenstialsXChat.jar" "${url}artifact/${path2}"
+    printf "\r${GREEN}:: ${NC}[$i/$maxi] ${BWHITE}Succesfully downloaded ${BLUE}EssenstialsXChat${BWHITE}!${NC}\n"
+    ((i += 1))
+}
 
-# Download EssenstialsXChat
-downloadLatestBuild "https://ci.ender.zone/job/EssentialsX/lastSuccessfulBuild/artifact/jars/" "EssenstialsXChat"
+downloadEssentialsX
 
 # Download Chunky
 downloadLatestBuild "https://ci.codemc.io/view/Author/job/pop4959/job/Chunky/lastSuccessfulBuild/artifact/bukkit/build/libs/" "Chunky"
@@ -93,4 +109,4 @@ downloadLatestFile "https://dev.bukkit.org/projects/player-heads/files/latest" "
 downloadLatestFile "https://dev.bukkit.org/projects/coreprotect/files/latest" "CoreProtect"
 
 # Download Graves
-downloadLatestFile "https://repo.ranull.com/maven/ranull/com/ranull/Graves/DEV/Graves-DEV.jar" "Graves"
+downloadLatestFile "https://repo.ranull.com/maven/ranull/com/ranull/graves/DEV/Graves-DEV.jar" "Graves"
