f30a59bc559da1a8f79dee96e6fa425363242c23
kamack38 <64226248+kamack38@users.noreply.github.com>
2023-07-16 00:26:05 +0200

    refactor: 💡 Use aikars flags
    - Remove incubator modules

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -95,12 +95,12 @@ if ($nonpremium) {
     }
     else {
         Write-Host "Running non-premium Minecraft server with $RAM GB of RAM..." -ForegroundColor Yellow
-        java ('-Xmx' + $MEM) ('-Xms' + $MEM) --add-modules=jdk.incubator.vector -jar purpur.jar --nogui --online-mode 0
+        java ('-Xmx' + $MEM) ('-Xms' + $MEM) -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper.jar --nogui --online-mode 0
     }
 }
 else {
     Write-Host "Running premium Minecraft server with $RAM GB of RAM..." -ForegroundColor Yellow
-    java ('-Xmx' + $MEM) ('-Xms' + $MEM) --add-modules=jdk.incubator.vector -jar purpur.jar --nogui
+    java ('-Xmx' + $MEM) ('-Xms' + $MEM) -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper.jar --nogui
 }
 
 if ($webhook) {
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 ENGINE_PATH="$(dirname "$0")/../src/purpur.jar"
 cd "$(dirname "$0")/../src/"
-java -Xmx4096M -Xms4096M --add-modules=jdk.incubator.vector -jar purpur.jar nogui
+java -Xms4G -Xmx4G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar purpur.jar --nogui
